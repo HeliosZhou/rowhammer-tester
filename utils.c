96a3b2d99d10a160af1a1e28b6df93cd164bc9ee
HeliosZhou <HeliosZhou@users.noreply.github.com>
2025-11-25 20:44:23 +0800

    Add sdram.c and utils.c files
    - sdram.c: SDRAM initialization and control functions
    - utils.c: Utility functions for memory operations

---
+++ utils.c
@@ -0,0 +1,74 @@
+// This file is Copyright (c) 2023 Antmicro <www.antmicro.com>
+// License: BSD
+
+#include <stdio.h>
+
+#include <liblitedram/utils.h>
+#include <liblitedram/sdram_spd.h>
+
+#include <libbase/i2c.h>
+
+#define KIB 1024
+#define MIB (KIB*1024)
+#define GIB (MIB*1024)
+
+void print_size(uint64_t size) {
+	if (size < KIB)
+		printf("%" PRIu64 "B", size);
+	else if (size < MIB)
+		printf("%" PRIu64 ".%" PRIu64 "KiB", size/KIB, (size/1   - KIB*(size/KIB))/(KIB/10));
+	else if (size < GIB)
+		printf("%" PRIu64 ".%" PRIu64 "MiB", size/MIB, (size/KIB - KIB*(size/MIB))/(KIB/10));
+	else
+		printf("%" PRIu64 ".%" PRIu64 "GiB", size/GIB, (size/MIB - KIB*(size/GIB))/(KIB/10));
+}
+
+void print_progress(const char * header, uint64_t origin, uint64_t size)
+{
+	printf("%s 0x%" PRIx64 "-0x%" PRIx64 " ", header, origin, origin + size);
+	print_size(size);
+	printf("   \r");
+}
+
+#ifdef CSR_SDRAM_BASE
+
+#include <generated/sdram_phy.h>
+
+uint64_t sdram_get_supported_memory(void) {
+#ifdef CONFIG_HAS_I2C
+
+#if defined(SDRAM_PHY_DDR3) || defined(SDRAM_PHY_DDR4)
+	uint8_t buf;
+
+	/* ZCU104: Configure I2C multiplexer before reading SPD for memory detection */
+	if (i2c_poll(0x74)) {
+		unsigned char mux_data = 0x80;
+		i2c_write(0x74, 0x00, &mux_data, 1, 1);
+	}
+
+	if (!sdram_read_spd(0x0, 4, &buf, 1, true)) {
+		/* Try SPD address 1 for ZCU104 */
+		if (!sdram_read_spd(0x1, 4, &buf, 1, true)) {
+			printf("Couldn't read SDRAM size from the SPD, defaulting to 256 MB.\n");
+			return 256 << 20;
+		}
+	}
+
+	/* minimal supported is 256 Mb */
+	uint64_t single_die_capacity  = 256 << 20;
+	single_die_capacity <<= buf & 0x7;
+
+	/* convert from bits to bytes (divide by 8) */
+	single_die_capacity >>= 3;
+
+	return SDRAM_PHY_MODULES * single_die_capacity;
+#else
+	return SDRAM_PHY_SUPPORTED_MEMORY;
+#endif
+
+#else /* no CONFIG_HAS_I2C */
+	return SDRAM_PHY_SUPPORTED_MEMORY;
+#endif /* CONFIG_HAS_I2C */
+}
+
+#endif
